7b0473fe5473865d277fcef44a2546e7cb1b9f5c
Jim Olson <jim.olson.456@gmail.com>
2023-05-15 15:57:52 -0500

    added lecture notes

---
+++ src/main/java/NameServlet.java
@@ -0,0 +1,8 @@
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "NameServlet", urlPatterns = "/name")
+public class NameServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) Throw ServletE
+}
